f2f76775570339753aa01fcc41a89d6bc2b1e1ee
corviato1 <44780076+corviato1@users.noreply.github.com>
2025-03-06 22:26:16 -0800

    Update About.js

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Bella Toka App</h1>
+            <p>Catchy explainer</p>
+            <img src="https://example.com/stats.png" alt="Stats" />
+            <p>Link on left goes to stats page</p>
+        </div>
+    );
+};
+
+export default About;
