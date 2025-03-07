ed14eef9c74257ce4e958c8d275d4c624ee57418
corviato1 <44780076+corviato1@users.noreply.github.com>
2025-03-06 22:27:52 -0800

    Update App.js

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import About from '../components/About';
+import Footer from '../components/Footer';
+
+const App = () => {
+    return (
+        <div>
+            <About />
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
