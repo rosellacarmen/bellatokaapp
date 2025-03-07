3665d78f1b65f9aca4a93924a3c4325809d774b2
corviato1 <44780076+corviato1@users.noreply.github.com>
2025-03-06 22:26:40 -0800

    Update Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Experience the future of cannabis data cultivation</p>
+            <button>
+                <img src="https://example.com/image.png" alt="Image" />
+            </button>
+        </footer>
+    );
+};
+
+export default Footer;
