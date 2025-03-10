5d90a8ca5f7c3232e63e1a521e1f517be84634cd
corviato1 <44780076+corviato1@users.noreply.github.com>
2025-03-10 10:36:36 -0700

    optimization attempt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,42 @@
 import React from "react";
 import "../styles/footer.css";
 
-const Contact = () => (
+const links = [
+  {
+    url: "enterprise.bellatoka.app/login",
+    imageSrc: `${process.env.PUBLIC_URL}/images/footer/enterprise.png`,
+    altText: "Enterprise Login"
+  },
+  {
+    url: "user.bellatoka.app/login",
+    imageSrc: `${process.env.PUBLIC_URL}/images/footer/user.png`,
+    altText: "User Login"
+  },
+  {
+    url: "https://github.com/corviato1/bellatokaapp",
+    imageSrc: `${process.env.PUBLIC_URL}/images/footer/github.png`,
+    altText: "GitHub Repository"
+  }
+];
+
+const Footer = () => (
   <div className="footer-container">
-    <a
-      href="enterprise.bellatoka.app/login"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img
-        src={`${process.env.PUBLIC_URL}/images/footer/enterprise.png`}
-        alt="Enterprise"
-        className="footer-icon"
-      />
-    </a>
-    <a
-      href="user.bellatoka.app/login"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img
-        src={`${process.env.PUBLIC_URL}/images/footer/user.png`}
-        alt="User"
-        className="footer-icon"
-      />
-    </a>
-    <a
-      href="https://github.com/corviato1/bellatokaapp"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img
-        src={`${process.env.PUBLIC_URL}/images/footer/github.png`}
-        alt="Github"
-        className="footer-icon"
-      />
-    </a>
+    {links.map((link, index) => (
+      <a
+        key={index} // Unique key for each link
+        href={link.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={link.imageSrc}
+          alt={link.altText}
+          title={link.altText}
+          className="footer-icon"
+        />
+      </a>
+    ))}
   </div>
 );
 
-export default Contact;
+export default Footer;
