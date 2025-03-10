c12fe04c89dbba53a135d251f73ce69d67a79be4
corviato1 <44780076+corviato1@users.noreply.github.com>
2025-03-10 10:50:45 -0700

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import Showcase from '../components/Showcase';
 import About from '../components/About';
 import Footer from '../components/Footer';
 
-const App = () => {
-    return (
-        <div>
-            <About />
-            <Footer />
-        </div>
-    );
-};
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Header />
+          <Showcase />
+          <About />
+          <Footer />
+        </Route>
+        {/* Add other routes as needed */}
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
