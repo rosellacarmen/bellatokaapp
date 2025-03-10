076891e41ab0b4f00814d1040966d27435cba23b
corviato1 <44780076+corviato1@users.noreply.github.com>
2025-03-10 10:08:17 -0700

    footer update
    images needed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Footer = () => {
-    return (
-        <footer>
-            <p>Experience the future of cannabis data cultivation</p>
-            <button>
-                <img src="https://example.com/image.png" alt="Image" />
-            </button>
-        </footer>
-    );
-};
+const Contact = () => (
+  <div className="footer-container">
+    <a
+      href="enterprise.bellatoka.app/login"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={`${process.env.PUBLIC_URL}/images/footer/enterprise.png`}
+        alt="Enterprise"
+        className="footer-icon"
+      />
+    </a>
+    <a
+      href="user.bellatoka.app/login"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={`${process.env.PUBLIC_URL}/images/footer/user.png`}
+        alt="User"
+        className="footer-icon"
+      />
+    </a>
+    <a
+      href="https://github.com/corviato1/bellatokaapp"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={`${process.env.PUBLIC_URL}/images/footer/github.png`}
+        alt="Github"
+        className="footer-icon"
+      />
+    </a>
+  </div>
+);
 
-export default Footer;
+export default Contact;
