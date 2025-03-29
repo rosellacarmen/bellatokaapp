e3a8a68ef85e42710091da86db13fcc8d0638652
corviato1 <44780076+corviato1@users.noreply.github.com>
2025-03-28 19:44:40 -0700

    lol
    lol, not doing that 265 times... may have to though.   idk. lol

--- src/images/strains/applescotti/index.js
+++ src/images/strains/applescotti/index.js
@@ -1,3 +1,3 @@
-const images = Array.from({length: 264}, (_, i) => `${String(i + 1).padStart(3, '0')}.jpg`);
+const images = Array.from({length: 264}, (_, i) => `${String(i + 1).padStart(3, '0')}.png`);
 
 export default images;
